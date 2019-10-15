1c2c7a57d458f2f78fb3a8bc4cbf0bc4cdfa2b9a
lepfsd-strapp <lperez@master.inmobilia.com>
2019-10-15 00:40:59 -0400

    async await 2

--- currency-convert.js
+++ currency-convert.js
@@ -8,6 +8,22 @@ const axios = require('axios');
 	})
 }; */
 
+/*const getCountries = (currencyCode) => {
+	return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
+		return response.data.map((country) => country.name);
+	});
+}*/
+
+/*const convertCurreny = (from2, to, amount) => {
+	let convertedAmount;
+	getExchangeRate(from2, to).then((rate) => {
+		convertedAmount = (amount * rate).toFixed(2);
+		return getCountries(to);
+	}).then((countries) => {
+		return `${amount} ${from2} is worth ${convertedAmount} ${to}. you cand spend in ${countries}`; 
+	});
+};*/
+
 const getExchangeRate = async (from2, to) => {
 	const response = await axios.get('http://data.fixer.io/api/latest?access_key=17c8b050e7685e9a3ead7d931b672868');
 
@@ -18,25 +34,32 @@ const getExchangeRate = async (from2, to) => {
 	
 };
 
-/*const getCountries = (currencyCode) => {
-	return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
-		return response.data.map((country) => country.name);
-	});
-}*/
-
 const getCountries = async (currencyCode) => {
 	const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
 	return response.data.map((country) => country.name);
 }
 
+const convertCurreny = async (from2, to, amount) => {
+	const rate = await getExchangeRate(from2, to);
+	const countries = await getCountries(to);
+	const convertedAmount = (amount * rate).toFixed(2);
+	return `${amount} ${from2} is worth ${convertedAmount} ${to}. you cand spend in ${countries}`; 
+}; 
+
 getExchangeRate('USD', 'CAD').then((rate) => {
-	console.log(rate);
+	//console.log(rate);
 }).catch((e) => {
 	console.log(e);
 }); 
 
 getCountries('VEF').then((countries) => {
-	console.log(countries);
+	//console.log(countries);
+}).catch((e) => {
+	console.log(e);
+});
+
+convertCurreny('USD', 'CAD', 20).then((message) => {
+	console.log(message);
 }).catch((e) => {
 	console.log(e);
 });
